658f91b0b6e94fb266590faf951c17dfbf841668
xdong <xdong@wandtec.com>
2021-01-20 20:56:56 +0800

    debug visa handler

--- instrument_IO/call_mainwindow.py
+++ instrument_IO/call_mainwindow.py
@@ -18,29 +18,70 @@ class MyForm(QMainWindow):
     def __init__(self):
 
         super().__init__()
+
+        # 定义visa 设备的 session属性，保证在窗口应用下持续性
         self.session = None
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         # 拒绝菜单栏的 mac 风格
         self.ui.menubar.setNativeMenuBar(False)
 
         self.ui.actionClose.triggered.connect(qApp.quit)
         self.ui.actionConnect.triggered.connect(self.get_visa_addr)
         self.ui.actionClear.triggered.connect(self.clear_logging)
-        self.ui.btn_send_command.clicked.connect(self.write_cmd)
-        self.ui.btn_read_response.clicked.connect(self.read_response)
-        self.ui.btn_send_then_read.clicked.connect(self.query_cmd)
+        # signal and slot ,transfer parameters by lambda
+        # self.ui.btn_write.clicked.connect(lambda: self.write_cmd(self.cmd))
+        # self.cmd = self.ui.comboBox_cmd_history.currentText()
+        self.ui.btn_write.clicked.connect(self.btn_handler)
+        self.ui.btn_read.clicked.connect(self.btn_handler)
+        self.ui.btn_query.clicked.connect(self.btn_handler)
 
         self.update_status_bar()
 
     def clear_logging(self):
         self.ui.textBrowser.clear()
 
-    def write_cmd(self):
+    def write_cmd(self, command=''):
+        """
+        :TODO 槽函数，传参
+        :TODO sender()
+        :param command:
+        :return:
+        """
+        self.update_logging('->' + command)
+        try:
+            self.session.write(command)
+        except (visa.VisaIOError, AttributeError) as ex:
+            self.exception_handler(ex)
         pass
 
-    def read_response(self):
-        pass
+    def btn_handler(self):
+
+        sender = self.sender()
+        self.cmd = self.ui.comboBox_cmd_history.currentText()
+
+        if sender.text() == 'Read':
+            self.update_logging('<--' + self.cmd)
+            try:
+                self.session.read(self.cmd)
+            except (visa.VisaIOError, AttributeError) as ex:
+                self.exception_handler(ex)
+        elif sender.text() == 'Write':
+            self.update_logging('-->' + self.cmd)
+            try:
+                self.session.write(self.cmd)
+            except (visa.VisaIOError, AttributeError) as ex:
+                self.exception_handler(ex)
+        elif sender.text() == 'Query':
+            self.update_logging('<-->' + self.cmd)
+            try:
+                self.session.query(self.cmd)
+            except (visa.VisaIOError, AttributeError) as ex:
+                self.exception_handler(ex)
+
+
 
     def query_cmd(self):
         pass
@@ -49,7 +90,7 @@ class MyForm(QMainWindow):
         """开启新的对话子窗口，通过文本输入框获得VIAS_ADDRESS,
         OK: 返回VIAS_ADDRESS，关闭对话框
         Cancel: 关闭对话框
-        :return
+        :return  None
         """
 
         dialog = ConnectDialog(self)
@@ -59,6 +100,7 @@ class MyForm(QMainWindow):
             visa_addr = dialog.VISA_addr()
         dialog.destroy()
 
+        # 创建主窗口visa session对象
         if visa_addr.strip():
             self.session = self.create_session(visa_addr)
 
@@ -73,7 +115,7 @@ class MyForm(QMainWindow):
 
     def create_session(self, visa_addr):
         """建立和设备的通信
-        :return 返回所连接设备的会话对象
+        :return 返回visa 设备的session对象
         """
         res_manager = visa.ResourceManager()
         self.update_status_bar('Connecting to ' + visa_addr)
@@ -91,7 +133,7 @@ class MyForm(QMainWindow):
             self.exception_handler(ex)
         return session
 
-    def update_logging(self, info=None):
+    def update_logging(self, info=''):
         """更新日志窗口"""
         self.ui.textBrowser.append(info)
 
