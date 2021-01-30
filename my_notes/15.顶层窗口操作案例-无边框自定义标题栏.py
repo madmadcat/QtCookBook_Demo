cadf5aa0b76504df6dafe65142096ce45f33f0f0
xdong <xdong@wandtec.com>
2021-01-30 17:15:27 +0800

    新增若干探索实验

--- my_notes/15.顶层窗口操作案例-无边框自定义标题栏.py
+++ my_notes/15.顶层窗口操作案例-无边框自定义标题栏.py
@@ -21,54 +21,76 @@ class MyFramelessWidget(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setup_ui()
-
-    def setup_ui(self):
         self.setWindowTitle('无边框窗口实践')
         self.setWindowOpacity(0.9)
         self.resize(400, 400)
-        # 设置窗口标志为无边框
         self.setWindowFlag(Qt.FramelessWindowHint, on=True)
+        self.y = 0
+        self.btn_w = 69
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        # 设置窗口标志为无边框
 
         # 设置窗口标志为显示最大最小化按钮
         # self.setWindowFlags(Qt.WindowMinMaxButtonsHint)
 
         # 为窗口添加标题栏和一个关闭按钮
-        self.setWindowFlags(Qt.WindowTitleHint)
+        # self.setWindowFlags(Qt.WindowTitleHint)
 
         # 添加三个子控件按钮 右上角
-        icon_close = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarCloseButton.value)
-        icon_max = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarMaxButton.value)
-        icon_min = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarMinButton.value)
+        # self.icon_close = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarCloseButton.value)
+        # self.icon_max = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarMaxButton.value)
+        # self.icon_min = QApplication.style().standardIcon(my_enum.SPIcon.SP_TitleBarMinButton.value)
 
-        y = 0
-        btn_w = 70
-        btn_close = QPushButton(QIcon(icon_close), '', self)
-        # btn_close = QPushButton('关闭', self)
-
-        btn_close_w = btn_close.width()
         window_w = self.width()
-        # btn_close_x = window_w - btn_close_w
-        btn_close_x = window_w - btn_w
-        btn_close.move(btn_close_x, y)
-        print(btn_close.rect())
-        print(btn_close.minimumSizeHint())
-        print(btn_close_w)
-        print('btnclos x', btn_close_x)
-        btn_max = QPushButton(QIcon(icon_max), '', self)
-        btn_max_x = btn_close_x - btn_w
-        btn_max.move(btn_max_x, y)
-        print('btnmax x', btn_max_x)
 
-        btn_min = QPushButton(QIcon(icon_min), '', self)
-        btn_min_x = btn_max_x - btn_w
-        btn_min.move(btn_min_x, y)
-        print('btnmin x', btn_min_x)
+        # self.btn_close = QPushButton(QIcon(self.icon_close), '', self)
+        self.btn_close = QPushButton('关闭', self)
+        btn_close_x = window_w - self.btn_w
+        self.btn_close.move(btn_close_x, self.y)
 
-        btn_close.clicked.connect(self.close)
-        btn_max.clicked.connect(self.showMaximized)
-        btn_min.clicked.connect(self.showMinimized)
-    pass
+        # self.btn_max = QPushButton(QIcon(self.icon_max), '', self)
+        self.btn_max = QPushButton('最大化', self)
+        btn_max_x = btn_close_x - self.btn_w
+        self.btn_max.move(btn_max_x, self.y)
+
+        # self.btn_min = QPushButton(QIcon(self.icon_min), '', self)
+        self.btn_min = QPushButton('最小化', self)
+        btn_min_x = btn_max_x - self.btn_w
+        self.btn_min.move(btn_min_x, self.y)
+
+        self.btn_close.clicked.connect(self.close)
+        self.btn_max.clicked.connect(self.slot_max)
+        self.btn_min.clicked.connect(self.showMinimized)
+
+    def slot_max(self):
+        if self.isMaximized():
+            self.showNormal()
+            self.btn_max.setText('最大化')
+
+        else:
+            self.showMaximized()
+            self.btn_max.setText('恢复')
+
+    def mousePressEvent(self, QMouseEvent) -> None:
+        # 判定点击的时候是鼠标左键
+        # 在此处设置一个标记， 用作判定是否需要移动
+        # 窗口的原始坐标
+        # 鼠标按下的点
+        pass
+
+    def mouseMoveEvent(self, QMouseEvent) -> None:
+        # if 窗口的移动标记 == True
+        # 根据鼠标按下的点 计算移动向量
+        # 根据移动向量，和窗口的原始坐标 = 最新坐标
+        # 移动整个窗口的位置
+        pass
+
+    def mouseReleaseEvent(self, QMouseEvent) -> None:
+        # 把这个标记，进行重置 False
+        pass
 
 
 if __name__ == '__main__':
