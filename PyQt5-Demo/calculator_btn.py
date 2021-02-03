3712e1c46c575f43bb667db9368d3a0fa0760f6b
xdong <xdong@wandtec.com>
2021-02-04 07:50:04 +0800

    顺子的实战案例练习-finished

--- PyQt5-Demo/calculator_btn.py
+++ PyQt5-Demo/calculator_btn.py
@@ -12,43 +12,55 @@ Created on 2021-02-2021/2/3
 
 from PyQt5.Qt import *
 
+
 class CalculatorBtn(QPushButton):
+
+    key_pressed = pyqtSignal(str, str)
 
     def __init__(self, parent=None, *args, **kwargs):
         super(CalculatorBtn, self).__init__(parent, *args, **kwargs)
 
+    def resizeEvent(self, *args, **kwargs) -> None:
+        super().resizeEvent(*args, **kwargs)
         self.setStyleSheet("""
-QPushButton[bg='gray'] {
-    color: white;
-    background-color: rgb(88, 88, 88);
-    border: 1px solid;
-}
-QPushButton[bg='gray']:hover {
-    background-color: rgb(150, 150, 150);
-}    
-QPushButton[bg='orange'] {
-    color: white;
-    background-color: rgb(207, 138, 0);
-    border: 1px solid;
-}    
-QPushButton[bg='orange']:hover {
-    background-color: rgb(238, 159, 0);
-}        
-QPushButton[bg='orange']:checked {
-    background-color: rgb(207, 100, 0);
-} 
-QPushButton[bg='lightgray'] {
-    color: black;
-    background-color: rgb(200, 200, 200);
-    border: 1px solid;
-}   
-QPushButton[bg='lightgray']:hover {
-    background-color: rgb(230, 230, 230);
-}
-QPushButton[bg] {
-    border-radius: %dpx;
-}
-        
+        QPushButton[bg='gray'] {
+            color: white;
+            background-color: rgb(88, 88, 88);
+            border: 1px solid;
+        }
+        QPushButton[bg='gray']:hover {
+            background-color: rgb(150, 150, 150);
+        }    
+        QPushButton[bg='orange'], QPushButton[bg='equal'] {
+            color: white;
+            background-color: rgb(207, 138, 0);
+            border: 1px solid;
+        }    
+        QPushButton[bg='orange']:hover, QPushButton[bg='equal']:hover {
+            background-color: rgb(238, 159, 0);
+        }        
+        QPushButton[bg='orange']:checked {
+            color: white;
+            background-color: rgb(207, 100, 0);
+        } 
+        QPushButton[bg='lightgray'] {
+            color: black;
+            background-color: rgb(200, 200, 200);
+            border: 1px solid;
+        }   
+        QPushButton[bg='lightgray']:hover {
+            background-color: rgb(230, 230, 230);
+        }
+        QPushButton[bg] {
+            border-radius: %dpx;
+            font-size: 20dpx
+        }
 
-        """ % (self.width() / 2))
-        print(self.width() / 2)
+
+        """ % (min(self.width(), self.height()) / 2))
+        # 圆角半径为控件宽或者高其中最小的那个值的一半
+
+    def mousePressEvent(self, *args, **kwargs):
+        # 重写mouse事件，发射自定义信号
+        super(CalculatorBtn, self).mousePressEvent(*args, **kwargs)
+        self.key_pressed.emit(self.text(), self.property('role'))
--- PyQt5-Demo/calculator_pane.py
+++ PyQt5-Demo/calculator_pane.py
@@ -16,12 +16,141 @@ from PyQt5.Qt import *
 from resource.calculator_ui import Ui_Form
 
 
+class Calculator(QObject):
+
+    signal_calc_result = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # 数字键位 num 运算符 operator
+        self.key_models = []
+
+
+    def calculate(self):
+
+        expression = ''
+
+        # if self.key_models[-1]['role'] == 'operator'
+        if len(self.key_models) <= 2:
+            return self.key_models[0]['title']
+        elif self.key_models[-1]['role'] == 'number':
+            for model in self.key_models:
+                expression += model['title']
+        else:
+            for model in self.key_models[:-1]:
+                expression += model['title']
+
+        # self.key_models = []
+        print(expression)
+        result = eval(expression)
+        return result
+
+    def parse_key_model(self, key_model):
+        """
+        1. 每次点击的键位都存入列表
+        2. 如果role是clear ，清空列表
+        3. 如果role是操作符，且操作符是第一个item，清空列表，否则确定第一个number。存入结果列表。
+            如果连续出现操作符，则替代上一个操作符
+        4. 如果是num，且是数字1-9 ，构造一个number， 如果下一个还是num，则按照十进制构造number
+        5. 如果是calculate。则遍历结果列表。进行计算
+
+        :param key_model:
+        :return:
+        """
+        # print(key_model)
+        if key_model['role'] == 'clear':
+            print('clear all data!')
+            self.key_models = []
+            self.signal_calc_result.emit('0.0')
+            return None
+
+        if key_model['role'] == 'calculate':
+            self.signal_calc_result.emit(str(self.calculate()))
+            return None
+
+        if len(self.key_models) == 0:
+            if key_model['role'] == 'number':
+                if key_model['title'] == '.':
+                    key_model['title'] = '0.'
+
+                if key_model['title'] in ('%', "+/-"):
+                    return None
+
+                self.key_models.append(key_model)
+                print(self.key_models)
+                self.signal_calc_result.emit(self.key_models[-1]['title'])
+
+            return None
+
+        if key_model['title'] in ('%', "+/-"):
+            if self.key_models[-1]['role'] != 'number':
+                return None
+            else:
+                if key_model['title'] == '%':
+                    self.key_models[-1]['title'] = str(float(self.key_models[-1]['title']) / 100)
+                    print(self.key_models)
+                    self.signal_calc_result.emit(self.key_models[-1]['title'])
+
+                    return None
+                else:
+                    self.key_models[-1]['title'] = str(float(self.key_models[-1]['title']) * -1)
+                    print(self.key_models)
+                    self.signal_calc_result.emit(self.key_models[-1]['title'])
+
+                    return None
+
+        if key_model['role'] == self.key_models[-1]['role']:
+            if key_model['role'] == 'number':
+                if key_model['title'] == '.' and self.key_models[-1]['title'].__contains__('.'):
+                    return None
+
+                if self.key_models[-1]['title'] != '0':
+                    self.key_models[-1]['title'] += key_model['title']
+                    print(self.key_models)
+                else:
+                    self.key_models[-1] = key_model
+                    print(self.key_models)
+
+                self.signal_calc_result.emit(self.key_models[-1]['title'])
+
+            if key_model['role'] == 'operator':
+                self.key_models.pop()
+                self.key_models.append(key_model)
+                print(self.key_models)
+                #
+                self.signal_calc_result.emit(str(self.calculate()))
+        else:
+            self.key_models.append(key_model)
+            if key_model['role'] == 'operator':
+                self.signal_calc_result.emit(str(self.calculate()))
+            else:
+                self.signal_calc_result.emit(key_model['title'])
+
+
 class CalculatorPane(QWidget, Ui_Form):
+
+    signal_calc_result = pyqtSignal(str)
 
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.setAttribute(Qt.WA_StyledBackground, True)
         self.setupUi(self)
+        print(self.lineEdit.layoutDirection())
+        # TODO: LE控件显示文本的方向问题没有解决
+        # self.lineEdit.setLayoutDirection(Qt.LeftToRight)
+        print(self.lineEdit.layoutDirection())
+
+        self.calculator = Calculator(self)
+
+        self.calculator.signal_calc_result.connect(self.slot_calc_result_relay)
+
+    def slot_calc_result_relay(self, result):
+        # self.signal_calc_result.emit(result)
+        self.lineEdit.setText(result)
+
+    def get_key(self, title, role):
+        self.calculator.parse_key_model({'title' : title, 'role' : role})
 
 
 if __name__ == '__main__':
